0d44a13cd9bc107e76a4b86081399e78fd3cba8b
LiLei <417608793@qq.com>
2015-10-24 20:01:53 +0800

    Recovery

--- images/Loader.js
+++ images/Loader.js
@@ -1,54 +1,54 @@
 // JavaScript Document
 var imgArray = [
-	"images/loading.gif",
-	"images/page1_bg.png",
-	"images/message_bg.png",
-	"images/swipe.png",
-	"images/swipe_light.png",
-	"images/hangup.png",
-	"images/mess_footer.png",
-	"images/messImage.png",
-	"images/mix.png",
-	"images/page2_bg.png",
-	"images/page3_bg2.png",
-	"images/page4_bg.png",
-	"images/page5_bg.png",
-	"images/page6_person.png",
-	"images/phonePhoto.png",
-	"images/toAnser.png",
-	"images/Tony.png",
-	"images/boss.png",
-	"images/weixin.png",
-	//"images/voice.mp3",
-	//"images/unlock.mp3",
-	//"images/wordBG2.mp3",
-	//"images/message.mp3",
-	//"images/call.mp3",
-	//"images/dudu.mp3",
-	"images/hangup.png",
-	"images/start.png",
-	"images/huadong01.png",
-	"images/huadong02.png",
-	"images/huadong03.png",
-	"images/huadong04.png",
-	"images/huadong05.png",
-	"images/huadong06.png",
-	"images/huadong07.png",
-	"images/huadong08.png",
-	"images/huadong09.png",
-	"images/huadong10.png",
+	"loading.gif",
+	"page1_bg.png",
+	"message_bg.png",
+	"swipe.png",
+	"swipe_light.png",
+	"hangup.png",
+	"mess_footer.png",
+	"messImage.png",
+	"mix.png",
+	"page2_bg.png",
+	"page3_bg2.png",
+	"page4_bg.png",
+	"page5_bg.png",
+	"page6_person.png",
+	"phonePhoto.png",
+	"toAnser.png",
+	"Tony.png",
+	"boss.png",
+	"weixin.png",
+	//"voice.mp3",
+	//"unlock.mp3",
+	//"wordBG2.mp3",
+	//"message.mp3",
+	//"call.mp3",
+	//"dudu.mp3",
+	"hangup.png",
+	"start.png",
+	"huadong01.png",
+	"huadong02.png",
+	"huadong03.png",
+	"huadong04.png",
+	"huadong05.png",
+	"huadong06.png",
+	"huadong07.png",
+	"huadong08.png",
+	"huadong09.png",
+	"huadong10.png",
 
-	"images/page7-bg.png",
-	"images/page7_gift.png",
-	"images/page7_giftnumber.png",
-	"images/page7_logo.png",
-	"images/page9-check.png",
-	"images/page9-psweye.png",
-	"images/page10_cover.png",
-	"images/newma.png",
-	"images/right.gif",
-	"images/toNext.png",
-	"images/share3.jpg"
+	"page7-bg.png",
+	"page7_gift.png",
+	"page7_giftnumber.png",
+	"page7_logo.png",
+	"page9-check.png",
+	"page9-psweye.png",
+	"page10_cover.png",
+	"newma.png",
+	"right.gif",
+	"toNext.png",
+	"share3.jpg"
 ];
 // 资源加载
 var Loader = function(){
@@ -82,7 +82,7 @@ var Loader = function(){
 						success();  // 回调函数
 					}
 				};
-				img.src = ctx + "/template/1024/" + imgArray[i];
+				img.src = "./images/" + imgArray[i];
 			}else{
 				this.loadMusic(imgArray[i]);
 			}
@@ -133,7 +133,7 @@ var Loader = function(){
 		img.onerror = function(){
 			obj.Loading(imgArray,obj.success);
 		};
-		img.src = ctx + "/template/1024/" + imgArray[0];
+		img.src = "./images/" + imgArray[0];
 	};
 };
 //window.onload = function(){
